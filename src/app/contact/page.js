92e49fcc89ce661b05f7bd560ae1c6edae7a3c50
AKHIL-DyC <akhiljee666@gmail.com>
2023-09-07 01:50:49 +0530

    working on contact

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import styles from './page.module.css'
-
+import Image from 'next/image'
+import conta from '/public/contact2.jpg'
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <h1>Wanna Keep in Touch ? </h1>
+      <h1 className={styles.txt}>Wanna Keep in Touch ? </h1>
       <div className={styles.content}>
-        <div className={styles.imgContainer}></div>
-        <form className={styles.form}></form>
+
+        <Image src={conta} className={styles.img}></Image>
+
+        <form className={styles.form}>
+          <input type="text" placeholder='name' className={styles.input} />
+          <input type="email" placeholder='email' className={styles.input} />
+          <textarea className={styles.textArea}
+            placeholder='message'
+            cols="30"
+            rows="10"></textarea>
+            <button className={styles.button}>Send</button>
+        </form>
       </div>
-      
+
     </div>
   )
 }
