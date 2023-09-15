c29884947bb30c4db8011ea566c9c103c19eb8af
AKHIL-DyC <akhiljee666@gmail.com>
2023-09-16 02:28:13 +0530

    added animation,responsive

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -9,50 +9,95 @@ import e17 from '/public/excel2017.png'
 import e19 from '/public/excel2019.png'
 import e21 from '/public/excel2021.png'
 import e22 from '/public/excel2022.svg'
-import { useInView, animated } from '@react-spring/web'
+import { useSpring,animated } from '@react-spring/web'
+import { useInView } from 'react-intersection-observer';
 
 
 
 export default function About() {
+ const[ref,inView]=useInView( {threshold:0.7});
+ const[springs,api]=useSpring(()=>({
+  from:{y:100},
+    opacity:0,
+  }))
+ 
+ if(inView){
+  api.start({
+    y:0,opacity:1
+  })
+ }
+ if(!inView){
+  api.start({
+    y:-100,opacity:0
+  })}
 
-  const [ref, springs] = useInView(
-    () => ({
-      from: {
-        opacity: 0,
-        y: 100,
-      },
-      to: {
-        opacity: 1,
-        y: 0,
-      },
+
+  const[ref2,inView2]=useInView();
+  const[springs2,api2]=useSpring(()=>({
+   from:{x:-100},
+     opacity:0,
+   }))
+  
+  if(inView2){
+   api2.start({
+     x:0,opacity:1
+   })
+  }
+  if(!inView2){
+   api2.start({
+     x:-100,opacity:0
+   })}
+
+   const[refi,inViewi]=useInView();
+   const[springsi,apii]=useSpring(()=>({
+    from:{x:100},
+      opacity:0,
     }))
-    const [refi, springsi] = useInView(
-      () => ({
-        from: {
-          opacity: 0,
-          x: 100,
-        },
-        to: {
-          opacity: 1,
-          x: 0,
-        },
-      }))
+   
+   if(inViewi){
+    apii.start({
+      x:0,opacity:1
+    })
+   }
+   if(!inViewi){
+    apii.start({
+      x:100,opacity:0
+    })}
 
+    const[ref3,inView3]=useInView();
+   const[springs3,api3]=useSpring(()=>({
+    from:{x:-100},
+      opacity:0,
+    }))
+   
+   if(inView3){
+    api3.start({
+      x:0,opacity:1
+    })
+   }
+   if(!inView3){
+    api3.start({
+      x:-100,opacity:0
+    })}
+
+
+  
   return (
     <div className={styles.about}>
+      
       <animated.div className={styles.img} ref={ref} style={springs}>
-        <Image src={mdl}></Image>
+        <Image src={mdl} styles={styles.mdl}></Image>
         </animated.div>
         <div className={styles.txt}>
         <p className={styles.disc}>In 2001, the students of Govt. Model Engineering College founded Mecel, a techno-managerial festival like no other. The annually celebrated techfest has steadily hosted students from more than 2000 universities and 1000 institutions across the nation as it has grown exponentially over the years. Mecel has always provided opportunities to students in ways they have only dreamt of, and everyone is invited to be a part of that journey to make this festival a grand success. Now, gearing up for its 23rd edition, Mecel promises an celebration of technology that’s a bigger and brighter blast than ever.</p>
         </div>
         <div className={styles.excel}>
-            <animated.div className={styles.imageContainer} ref={refi} style={springsi}><Image src={e15} className={styles.imgs} alt="excel"></Image></animated.div>
+            <animated.div className={styles.imageContainer} ref={ref2} style={springs2}><Image src={e15} className={styles.imgs} alt="excel"></Image></animated.div>
             <animated.div className={styles.imageContainer} ref={refi} style={springsi}><Image src={e16} className={styles.imgs} alt="excel"></Image></animated.div>
-            <animated.div className={styles.imageContainer} ref={refi} style={springsi}><Image src={e17} className={styles.imgs} alt="excel"></Image></animated.div>
+            <animated.div className={styles.imageContainer} ref={ref2} style={springs2}><Image src={e17} className={styles.imgs} alt="excel"></Image></animated.div>
             <animated.div className={styles.imageContainer} ref={refi} style={springsi}><Image src={e19} className={styles.imgs} alt="excel"></Image></animated.div>
-            <animated.div className={styles.imageContainer} ref={refi} style={springsi}><Image src={e21} className={styles.imgs} alt="excel"></Image></animated.div>
-            <animated.div className={styles.imageContainer} ref={refi} style={springsi}><Image src={e22} className={styles.imgs} alt="excel"></Image></animated.div>
+            <animated.div className={styles.imageContainer} ref={ref3} style={springs3}><Image src={e21} className={styles.imgs} alt="excel"></Image></animated.div>
+            <animated.div className={styles.imageContainer} ref={ref3} style={springs3}><Image src={e22} className={styles.imgs} alt="excel"></Image></animated.div>
         </div>
     </div>
   )
--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,20 +1,102 @@
+"use client"
 import React from 'react'
 import styles from './page.module.css'
 import Link from 'next/link'
-const page = () => {
+import Image from 'next/image'
+import { useSpring, animated } from '@react-spring/web';
+import { useInView } from 'react-intersection-observer';
+
+import car from '/public/carc.avif'
+import marathon from '/public/marathon.avif'
+import music from '/public/showsc.avif'
+import coding from '/public/hack.avif'
+import robo from '/public/robo.webp'
+import foot from '/public/foot.avif'
+import pcb from '/public/pcb.jpg'
+import ar from 'public/ardev.jpg'
+import web3 from 'public/web3.jpg'
+export default function page(){
+  const [ref, inView] = useInView({threshold:0.5});
+  const [springs, api] = useSpring(() => ({
+    from: { x: -100},
+    opacity:0,
+  }));
+
+  // Attach the onInView trigger
+  if (inView) {
+    api.start({ x: 0 ,opacity:1});
+  }
+  if (!inView) {
+    api.start({ x: -100 ,opacity:0});
+  }
+  const [ref2, inView2] = useInView({threshold:0.5});
+  const [springs2, api2] = useSpring(() => ({
+    from: { x: -100},
+    opacity:0,
+  }));
+
+  // Attach the onInView trigger
+  if (inView2) {
+    api2.start({ x: 0 ,opacity:1});
+  }
+  if (!inView2) {
+    api2.start({ x: -100 ,opacity:0});
+  }
+
+  const [ref3, inView3] = useInView({threshold:0.5});
+  const [springs3, api3] = useSpring(() => ({
+    from: { x: -100},
+    opacity:0,
+  }));
+
+  // Attach the onInView trigger
+  if (inView3) {
+    api3.start({ x: 0 ,opacity:1});
+  }
+  if (!inView3) {
+    api3.start({ x: 100 ,opacity:0});
+  }
   return (
-    <div className={styles.main}>
-      <div className={styles.talks}>
-        <Link href="/events/shows">SHOWS</Link>
+   <> 
+      
+      <animated.div className={styles.first} ref={ref} style={{...springs}}>
+      <Link href="/events/shows">
+        <div className={styles.talks}>
+        <p>EVENTS</p>
+        </div>
+          <div className={styles.imgContainer}>
+          <Image src={car} alt='car'className={styles.img}></Image>
+          <Image src ={marathon}alt='marathon'className={styles.img}></Image>
+          <Image src ={music}alt='concert'className={styles.img}></Image>
       </div>
+      </Link>
+      </animated.div> 
+
+      <animated.div className={styles.second}ref={ref2} style={{...springs2}}>
+      <Link href="/events/competition">
       <div className={styles.talks}>
-      <Link href="/events/competition">COMPETITION</Link>
+      <p>COMPETITION</p>
       </div>
+      <div className={styles.imgContainer}>
+        <Image src={coding} alt='coding' className={styles.img}></Image>
+        <Image src={robo} alt='robowar' className={styles.img}></Image>
+        <Image src={foot} alt='football' className={styles.img}></Image>
+      </div>
+      </Link>
+      </animated.div>
+
+      <animated.div className={styles.third}ref={ref3} style={{...springs3}}>
+      <Link href="/events/workshop">
       <div className={styles.talks}>
-      <Link href="/events/workshop">WORKSHOPS</Link>
+      <p> WORKSHOPS</p>
       </div>
-    </div>
+      <div className={styles.imgContainer}>
+        <Image src={pcb} alt='coding' className={styles.img}></Image>
+        <Image src={ar} alt='robowar' className={styles.img}></Image>
+        <Image src={web3} alt='football' className={styles.img}></Image>
+      </div>
+      </Link>
+      </animated.div>
+    </>
   )
 }
-
-export default page
